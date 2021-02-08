47b1960592f1074ee578d2b04de5cb16002bf0af
Sean Grant <sgrant177@yahoo.com>
2021-02-08 11:04:32 -0500

    inital commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Card from "../components/Card";
+import Form from "../components/Form";
+import Book from "../components/Book";
+import Footer from "../components/Footer";
+import API from "../utils/API";
+import { Col, Row, Container } from "../components/Grid";
+import { List } from "../components/List";
+
+class Home extends Component {
+  //initalize state with a books array, "q", and message
+
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  getBooks = () => {
+    // call the api.getBooks() and set it to state.
+  };
+
+  handleFormSubmit = event => {
+    event.preventDefault();
+    this.getBooks();
+  };
+
+  handleBookSave = id => {
+    const book = this.state.books.find(book => book.id === id);
+
+    API.saveBook({
+      googleId: book.id,
+      title: book.volumeInfo.title,
+      // use the googleController.js methods to save the data
+    }).then(() => this.getBooks());
+  };
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col size="md-12">
+            <Jumbotron>
+              <h1 className="text-center">
+                <strong>(React) Google Books Search</strong>
+              </h1>
+              <h2 className="text-center">Search for and Save Books of Interest.</h2>
+            </Jumbotron>
+          </Col>
+          <Col size="md-12">
+            <Card title="Book Search" icon="far fa-book">
+              <Form
+                handleInputChange={this.handleInputChange}
+                handleFormSubmit={this.handleFormSubmit}
+                q={this.state.q}
+              />
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col size="md-12">
+            <Card title="Results">
+              {this.state.books.length ? (
+                <List>
+                  {this.state.books.map(book => (
+                    <Book
+                      key={book.id}
+                      title={book.volumeInfo.title}
+                      // pass the rest of the book object as a prop
+                      Button={() => (
+                        <button
+                          onClick={() => this.handleBookSave(book.id)}
+                          className="btn btn-primary ml-2"
+                        >
+                          Save
+                        </button>
+                      )}
+                    />
+                  ))}
+                </List>
+              ) : (
+                <h2 className="text-center">{this.state.message}</h2>
+              )}
+            </Card>
+          </Col>
+        </Row>
+        <Footer />
+      </Container>
+    );
+  }
+}
+
+export default Home;
